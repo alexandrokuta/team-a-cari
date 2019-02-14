cfcca4d5badbeffb618dcd586ffb1bbb47ff517f
tanvk1ch1 <moko2.ss0828@gmail.com>
2019-02-14 17:10:10 +0900

    再デプロイ用
    what
    接続先を変更した
    why
    EC2インスタンスの再設定、ErasticIPを再取得したため

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.194.135.75', user: 'ec2-user', roles: %w{app db web}
+server '13.112.122.153', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
